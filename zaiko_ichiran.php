f0d98a14fa5320b6e1446680f34e455ecbdd3cc9
AkihitoItoga <190252lb@yse-c.net>
2020-10-07 12:13:38 +0900

    12コメント追加

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -85,7 +85,8 @@ if (/* ②の処理を書く */){
 							//⑪extract変数を使用し、1レコードのデータを渡す。
 
 							echo "<tr id='book'>";
-							echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
+							echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する?
+						 */."></td>";
 							echo "<td id='id'>$id</td>";
 							echo "<td id='title'>$title</td>";
 							echo "<td id='author'>$author</td>";
